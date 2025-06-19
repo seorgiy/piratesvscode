8af29a2671f9970ba64c61669379e9c5938f77e2
Seorgiy <george.sergeev2014@gmail.com>
2025-06-19 13:18:20 +0000

    init

--- src/translater.ts
+++ src/translater.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import * as fs from 'fs';
+import { parse } from 'ini';
 import * as vscode from 'vscode';
 
 interface LocFile {
@@ -19,7 +20,7 @@ export class Translater {
         locName = key[1].split("_")[0];
         locals = this.initLocFile(language, locName);
       }
-      return `"${locals[key[1]]}"`;
+      return `"${locals[key[1].toLowerCase()]}"`;
     });
 
     return modifiedSentence;
@@ -41,27 +42,31 @@ export class Translater {
     }
 
     const filepath = editor.document.fileName.split("Program/");
+    fileName = this.capitalizeFirstLetter(fileName);
     const rootPath = filepath[filepath.length - 2];
-    const filePath = path.join(rootPath, "Resource", "INI", "texts", language, "Localization_Assets", `${this.capitalizeFirstLetter(fileName)}.txt`);
+    const iniPath = path.join(rootPath, "Resource", "INI", "texts", "Localization_Assets.ini");
+    const iniContent = fs.readFileSync(iniPath, 'utf-8');
+    const config = parse(iniContent);
+    const filePath = path.join(rootPath, "Resource", "INI", "texts", language, path.join(...config[fileName].split("\\")), `${fileName}.txt`);
 
-    if (!fs.existsSync(filePath)) {
+    if (!fs.existsSync(filePath)) { 
       vscode.window.showErrorMessage(`Файл не найден: ${filePath}`);
       return {};
     }
 
     const content = fs.readFileSync(filePath, { encoding: 'utf8', flag: 'r' });
-    this.fillLocFile(result, content, fileName);
+    this.fillLocFile(result, content);
 
     return result;
   };
 
   // Разбиваем сначала по }, потом по { и убираем лишние символы, чтобы получить key: value тупа
-  fillLocFile = (locals: LocFile, text: string, _fileName: string) => {
+  fillLocFile = (locals: LocFile, text: string) => {
     let array: string[] = text.split("}");
     array.pop();
     array.forEach(x => {
       let array2: string[] = x.split("{");
-      locals[array2[0].trim()] = array2[1].trim();
+      locals[array2[0].trim().toLowerCase()] = array2[1].trim();
     });
   };
 
